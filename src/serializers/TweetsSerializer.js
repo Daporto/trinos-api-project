6d3370a494bdafb66c57e0d2bcfc1220caea58a0
magdaniele <magdaniele@uninorte.edu.co>
2021-11-24 19:56:16 -0500

    Style: :art: line sequence changed

--- src/serializers/TweetsSerializer.js
+++ src/serializers/TweetsSerializer.js
@@ -4,8 +4,6 @@ class TweetsSerializer extends BaseSerializer {
   constructor(models, commentsData, userData, paginationInfo) {
     const serializedModels = models.map((model) => {
       const serializedModel = model.toJSON();
-      console.log("User.Datas")
-      console.log(userData);
 
       serializedModel.user = userData.find(item => item.id === serializedModel.userId);
       delete serializedModel.user.password;
